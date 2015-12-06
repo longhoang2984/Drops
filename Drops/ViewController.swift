0a53ff18fef9d72bea0d027d5f49624cf041dce8
devJoshLopez <joshlopez@joshs-imac.local>
2015-12-06 12:47:10 -0800

    Initial Commit

---
+++ Drops/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drops
+//
+//  Created by Josh Lopez on 12/6/15.
+//  Copyright © 2015 Josh Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DropsTests/DropsTests.swift
@@ -0,0 +1,36 @@
+//
+//  DropsTests.swift
+//  DropsTests
+//
+//  Created by Josh Lopez on 12/6/15.
+//  Copyright © 2015 Josh Lopez. All rights reserved.
+//
+
+import XCTest
+@testable import Drops
+
+class DropsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
