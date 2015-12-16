97cb00c6f4087f7e92c695247436085f4338b05b
devJoshLopez <joshlopez@Joshs-iMac.local>
2015-12-16 12:11:34 -0800

    Have weatherUpdates working

--- Drops/UpdateViewController.swift
+++ Drops/UpdateViewController.swift
@@ -12,18 +12,21 @@ import Parse
 class UpdateViewController: UIViewController {
     
     @IBOutlet var longPressLabel: UILabel!
+    
+    private var weatherValue: Int?
+    
+    var timer: NSTimer?
+    
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // PFUser.logOut()
         
         // Check to see if a user is logged in
         let currentUser = PFUser.currentUser()
         if currentUser != nil {
             // Do stuff with the user
+            print("User is logged in, send to update screen")
         } else {
             // Show the login screen
             print("User not logged in, send to login screen")
@@ -32,9 +35,7 @@ class UpdateViewController: UIViewController {
             }
         }
         
-        
-        
-        }
+    }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
@@ -55,12 +56,72 @@ class UpdateViewController: UIViewController {
         
         if (sender.state == UIGestureRecognizerState.Ended) {
             print("Long Press Ended")
+            // self.timer!.invalidate()
+            
+            if self.longPressLabel.text == "Sunny" {
+                print("Sunny")
+                weatherValue = 1
+                
+            } else if self.longPressLabel.text == "Partly Cloudy" {
+                print("Partly Cloudy")
+                weatherValue = 2
+                
+            } else if self.longPressLabel.text == "Cloudy" {
+                print("Cloudy")
+                weatherValue = 3
+                
+            } else if self.longPressLabel.text == "Raining" {
+                print("Raining")
+                weatherValue = 4
+                
+            } else if self.longPressLabel.text == "Thunderstorm" {
+                print("Thunderstorm")
+                weatherValue = 5
+                
+            }
+            
+            let newWeatherUpdate = WeatherUpdate(author: PFUser.currentUser()!, weatherValue: weatherValue!)
+            newWeatherUpdate.saveInBackgroundWithBlock { (success, error) -> Void in
+                if error == nil {
+                    print("Post to parse successful")
+                } else {
+                    print("\(error?.localizedDescription)")
+                }
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+            
+            self.longPressLabel.text = "tap and hold to update"
+            self.timer!.invalidate()
             
         } else if (sender.state == UIGestureRecognizerState.Began) {
             print("Long Press Began")
             
+            self.startAnimatingLabel()
         }
     }
     
+    // MARK: - Weather Animation
+    var weatherStrings = ["Sunny", "Partly Cloudy", "Cloudy", "Raining", "Thunderstorm"]
+    func animateLabel() {
+        var i = 0
+        for weatherString in weatherStrings {
+            if self.longPressLabel.text == weatherString {
+                ++i
+                if i == weatherStrings.count {
+                    i = 0
+                }
+            
+            self.longPressLabel.text = weatherStrings[i]
+            break
+            }
+        
+            ++i
+        }
+    }
+    
+    func startAnimatingLabel() {
+        self.longPressLabel.text = weatherStrings.first
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: ("animateLabel"), userInfo: nil, repeats: true)
+    }
     
 }
